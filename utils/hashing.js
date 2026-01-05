c4fea7591dc2b1ff7a91a120ceab4d1495623847
saurabh8883 <saurabh.tripathi040820@gmail.com>
2026-01-05 22:26:53 +0530

    Initial Auth Project setup

---
+++ utils/hashing.js
@@ -0,0 +1,19 @@
+const { createHmac } = require('crypto');
+const bcrypt = require('bcryptjs');
+
+exports.doHash = async (value, saltValue) => {
+  const result = await bcrypt.hash(value, saltValue);
+  return result;
+};
+
+exports.doHashValidation = async (value, hashedValue) => {
+  const result = await bcrypt.compare(value, hashedValue);
+  return result;
+};
+
+exports.hmacProcess = (value, key) => {
+  const result = createHmac('sha256', key)
+    .update(value)
+    .digest('hex');
+  return result;
+};
